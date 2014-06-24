a36c49257294ea34a4cc74c0d23ca266392406be
ssameng <sam.eng@icloud.com>
2014-06-24 14:44:23 -0400

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+	public float speed;
+	public float jumpPower;
+
+	private CharacterController cController;
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Cube!");
+		cController = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 moveDirection = new Vector3(.1f, 0, 0);
+		cController.Move(moveDirection);
+		//Debug.Log("Cube!");
+
+	}
+}
+
+
+
+
